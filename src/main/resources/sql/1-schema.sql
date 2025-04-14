d1ee256c4bbda9ad8adc70dea5710058037722a7
Nguyễn Phi Long <22130152@st.hcmuaf.edu.vn>
2025-04-14 23:06:36 +0700

    update schema

--- src/main/resources/sql/1-schema.sql
+++ src/main/resources/sql/1-schema.sql
@@ -1,3 +1,6 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'rootpass';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 CREATE TABLE categories
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
